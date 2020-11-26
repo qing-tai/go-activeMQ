7584812901ae181560e5d72ac0029cc4561e0bdb
wujunbo <525690313@qq.com>
2020-11-26 11:39:55 +0800

    a  config

---
+++ active/init.go
@@ -0,0 +1,9 @@
+package active
+
+import "fmt"
+
+// Init func
+func Init() {
+	fmt.Println("active mq start")
+	// git config file
+}
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"go-activeMQ/active"
+	"os"
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+// read config
+func configInit() {
+	// Get config from json
+	// Get config from json
+	viper.SetConfigName("config/config")
+
+	viper.AddConfigPath(".")
+	viper.SetConfigType("json")
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Println("config file error: " + err.Error())
+		os.Exit(1)
+	}
+}
+
+func main() {
+	fmt.Println("start activeMQ client at: ", time.Now().Format("2006-01-02 15:04:05"))
+	configInit()
+	active.Init()
+}
